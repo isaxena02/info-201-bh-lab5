74c2cb29edc995d945f9c91236103d64712336fc
“isaxena02” <ishita.saxena1103@gmail.com>
2022-05-04 03:38:06 -0700

    added files

---
+++ lab6.R
@@ -0,0 +1,53 @@
+library(dplyr)
+
+# load the data from the csv file located inside this repository and
+# put it in a variable called `any_drinking'
+
+# find the number of rows and columns in this dataset
+
+
+## Create a data frame that has the `state` and `location` columns, and all columns with data from 2012
+
+
+## Using the 2012 data, create a column that has the difference in male and female drinking patterns
+
+
+## Create a *dataframe* of the locations, states, and differences for all locations where females drink more than males
+## (no extra columns).
+## Are there any locations where females drink more than males?
+## Comment your result (as a comment)
+
+
+## What is the location in which male and female drinking rates are most similar
+## (*absolute* difference is smallest)?
+## Your answer should be a *dataframe* of the location, state, and value of interest
+## (no extra columns)
+
+
+## As you've (hopefully) noticed, the `location` column includes national, state, and county level
+## estimates.  However, many audiences may only be interested in the *state* level data.
+## Given that, you should do the following:
+## * Create a new data frame that is only the state level observations in 2012
+## * For the sake of this analysis, you should treat Washington D.C. as a *state*
+##
+## Hint: check the 'state' and 'location' variables and think how to extract only
+## state-level data
+
+
+## Which state had the **highest** drinking rate for both sexes combined? 
+## Your answer should be a *dataframe* of the state and value of interest (no extra columns)
+##
+## For your check: this is Vermont
+
+
+
+## Which state had the **lowest** drinking rate for both sexes combined?
+## Your answer should be a *dataframe* of the state and value of interest (no extra columns)
+
+
+
+## What was the difference in (any-drinking) prevalence between the state with the highest level of
+## consumption, and the state with the lowest level of consumption?
+## Your answer should be a single value.
+
+
